8209db1bbb941703907f3e26bff52049de991032
Ruben Garat <ruben.garat@gemserk.com>
2012-02-14 18:09:07 -0200

    added basic api to get user data with username and privatekey not mapped to url yet

--- app/controllers/api/Users.java
+++ app/controllers/api/Users.java
@@ -26,4 +26,26 @@ public class Users extends Controller {
 		renderJSON(userDTO);
 	}
 	
+	static public void getUserInfo() {
+		String username = params.get("username");
+		if(username==null || username.equals(""))
+			error(400, "username is required");
+				
+		String privatekey = params.get("privatekey");
+		
+		if(privatekey==null || privatekey.equals(""))
+			error(400, "privatekey is required");
+		
+		User user = User.find("byUsername", username).first();
+		
+		if(user==null)
+			error(404, "the user " + username + " doesn't exist");
+		
+		if(!user.privatekey.equals(privatekey))
+			error(401, "error authenticating the user");
+		
+		UserDTO userDTO = new UserDTO(user.username, user.name, user.privatekey);
+		renderJSON(userDTO);
+	}
+	
 }
