3dab97146885aff96936377ee3c78537d7430432
Ariel Coppes <ariel.coppes@gemserk.com>
2012-02-24 15:38:47 -0200

    fixed data of some tests

--- test/controllers/services/LeaderboardServiceTest.java
+++ test/controllers/services/LeaderboardServiceTest.java
@@ -28,7 +28,7 @@ public class LeaderboardServiceTest {
 		score.day = day;
 		return score;
 	}
-	
+
 	private Score score(int year, int month, int week, int day) {
 		Score score = new Score();
 		score.year = year;
@@ -73,9 +73,9 @@ public class LeaderboardServiceTest {
 	public void shouldBeConsideredForBestOfMonth() {
 		Score[] scores = new Score[] { //
 		score(1, 1, 1, 1, 1, 500), //
-				score(1, 1, 1, 1, 2, 250) //
+				score(1, 2, 5, 80, 2, 250), //
 		};
-		Score score = score(1, 1, 1, 1, 1, 350);
+		Score score = score(1, 2, 6, 81, 1, 350);
 		Result result = LeaderboardService.calculateScoreScopeAndStuff(score, scores);
 		assertThat(score.scope, equalTo(2));
 		assertThat(result.replacedScores, equalTo(1));
@@ -83,18 +83,22 @@ public class LeaderboardServiceTest {
 
 	@Test
 	public void shouldNotBeConsideredAsBestOfMonth() {
-		Score[] scores = new Score[] { score(1, 1, 1, 1, 1, 500), score(1, 1, 1, 1, 2, 450) };
-		Score score = score(1, 1, 1, 1, 1, 350);
+		Score[] scores = new Score[] { //
+				score(1, 1, 1, 1, 1, 500), //
+				score(1, 2, 5, 80, 2, 450), //
+		};
+		Score score = score(1, 2, 5, 80, 1, 350);
 		Result result = LeaderboardService.calculateScoreScopeAndStuff(score, scores);
+		assertThat(result.todayScoreExists, equalTo(true));
 		assertThat(result.replacedScores, equalTo(-1));
 	}
 
 	@Test
 	public void shouldBeBestOfTodayButNotYesterday() {
 		Score[] scores = new Score[] { score(1, 1, 1, 1, 1, 500), //
-				score(1, 1, 1, 2, 2, 450), //
-				score(1, 1, 1, 3, 3, 350), //
-				score(1, 1, 1, 4, 4, 250) //
+				score(1, 1, 1, 2, 4, 450), //
+				score(1, 1, 1, 3, 4, 350), //
+				score(1, 1, 1, 4, 4, 250), //
 		};
 		Score score = score(1, 1, 1, 5, 1, 150);
 		Result result = LeaderboardService.calculateScoreScopeAndStuff(score, scores);
@@ -180,23 +184,23 @@ public class LeaderboardServiceTest {
 	@Test
 	public void testCalculateCandidateScope1() {
 		assertThat(LeaderboardService.calculateCandidateScopeForWorseScore( //
-				score(1, 1, 1, 1),  //
-				score(2, 12, 12, 12)), // 
+				score(1, 1, 1, 1), //
+				score(2, 12, 12, 12)), //
 				equalTo(Range.Month.scope));
-		
+
 		assertThat(LeaderboardService.calculateCandidateScopeForWorseScore( //
-				score(1, 1, 1, 1),  //
-				score(1, 1, 1, 2)), // 
+				score(1, 1, 1, 1), //
+				score(1, 1, 1, 2)), //
 				equalTo(Range.Day.scope));
-		
+
 		assertThat(LeaderboardService.calculateCandidateScopeForWorseScore( //
-				score(1, 1, 1, 1),  //
-				score(1, 1, 2, 8)), // 
+				score(1, 1, 1, 1), //
+				score(1, 1, 2, 8)), //
 				equalTo(Range.Week.scope));
-		
+
 		assertThat(LeaderboardService.calculateCandidateScopeForWorseScore( //
-				score(1, 1, 1, 1),  //
-				score(1, 2, 4, 32)), // 
+				score(1, 1, 1, 1), //
+				score(1, 2, 4, 32)), //
 				equalTo(Range.Month.scope));
 	}
 
